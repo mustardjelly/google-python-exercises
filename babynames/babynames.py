c53945c6fc4c61267644d865af993bc1c97ebed0
= <=>
2024-04-08 18:45:11 +1200

    baby names wip

--- babynames/babynames.py
+++ babynames/babynames.py
@@ -8,6 +8,7 @@
 
 import sys
 import re
+from typing import List
 
 """Baby Names exercise
 
@@ -34,35 +35,76 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
-def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  # +++your code here+++
-  return
+
+def read_file(filename: str) -> List[str]:
+    with open(filename, encoding="utf-8") as f:
+        contents = f.readlines()
+
+    return contents
+
+
+def read_year(contents: List[str]) -> str:
+    for line in contents:
+        year_line = re.match(r"<h3 align=\"center\">Popularity in (\d{4})</h3>", line)
+        if year_line is not None:
+            year = year_line.group(1)
+            break
+    return year
+
+
+def read_names(contents: List[str]) -> List[str]:
+    print(print("{0:<4} {1:<20} {2:<20}".format("Rank", "Male", "Female")))
+    for line in contents:
+        name_line = re.match(
+            r"<tr align=\"right\"><td>({\d+})</td><td>(\w+)</td><td>(\w+)</td>", line
+        )
+        if name_line is None:
+            continue
+
+        rank = name_line.group(1)
+        male = name_line.group(2)
+        female = name_line.group(3)
+
+        print("{0:<5} {1:<20} {2:<20}".format(rank, male, female))
+
+
+def extract_names(filename: str):
+    """
+    Given a file name for baby.html, returns a list starting with the year string
+    followed by the name-rank strings in alphabetical order.
+    ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+    """
+    contents = read_file(filename)
+    year = read_year(contents)
+    read_names(contents)
+    # names =
+    # +++your code here+++
+    return
 
 
 def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
+    # This command-line parsing code is provided.
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
+    args.append("/home/powell/google-python-exercises/babynames/baby1990.html")
 
-  if not args:
-    print('usage: [--summaryfile] file [file ...]')
-    sys.exit(1)
+    if not args:
+        print("usage: [--summaryfile] file [file ...]")
+        sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+    # Notice the summary flag and remove it from args if it is present.
+    summary = False
+    if args[0] == "--summaryfile":
+        summary = True
+        del args[0]
 
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
+    # +++your code here+++
+    # For each filename, get the names, then either print the text output
+    # or write it to a summary file
+    filename = args[0]
+    extract_names(filename)
 
-if __name__ == '__main__':
-  main()
+
+if __name__ == "__main__":
+    main()
